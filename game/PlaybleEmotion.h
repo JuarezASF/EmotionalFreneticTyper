b576768c43c024d9096d1f1fc44ff0c910ec184b
Juarez ASF <juarez.asf@gmail.com>
2016-05-22 19:30:05 -0300

    removing old game logic

---
+++ game/PlaybleEmotion.h
@@ -0,0 +1,14 @@
+//
+// Created by jasf on 5/22/16.
+//
+
+#ifndef IDJ201601T1_PLAYBLEEMOTION_H
+#define IDJ201601T1_PLAYBLEEMOTION_H
+
+
+class PlaybleEmotion {
+
+};
+
+
+#endif //IDJ201601T1_PLAYBLEEMOTION_H
---
+++ game/TyperInput.h
@@ -0,0 +1,14 @@
+//
+// Created by jasf on 5/22/16.
+//
+
+#ifndef IDJ201601T1_TYPERINPUT_H
+#define IDJ201601T1_TYPERINPUT_H
+
+
+class TyperInput {
+
+};
+
+
+#endif //IDJ201601T1_TYPERINPUT_H
